4dfc94295840c2b084b8f43771acce8350429385
Jeremy Larose <4101416@keranna.qc.ca>
2024-11-27 17:22:05 -0500

    Re-cleaned code for implementation of commands

--- commands/rotate.py
+++ commands/rotate.py
@@ -8,15 +8,29 @@ from utils.safecommand import SafeCommand
 class Extend(SafeCommand):
     delay = autoproperty(0.3)
 
+<<<<<<< Updated upstream
     def __init__(self, pivot: Pivot):
         super().__init__()
         self.pivot = pivot
+=======
+    def __init__(self, turret: Turret, speed:int, stick: wpilib.Joystick):
+        super().__init__()
+        self.turret = turret
+        self.stick = stick
+        self.speed = speed
+>>>>>>> Stashed changes
 
     def initialize(self):
         pass
 
+<<<<<<< Updated upstream
+=======
+    def turn(self):
+        pass
 
-    def isFinished(self):
+>>>>>>> Stashed changes
+
+    def finished(self):
         pass
 
     def end(self):
