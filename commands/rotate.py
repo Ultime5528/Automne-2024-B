fbf6c3cba8f41d937c6c95eedf9169a5bfc7c908
Jeremy Larose <4101416@keranna.qc.ca>
2024-11-27 17:26:25 -0500

    Fixed errors caused by mistake in rotate.py

--- commands/rotate.py
+++ commands/rotate.py
@@ -1,6 +1,7 @@
 import wpilib
 
 from subsystems.pivot import Pivot
+from subsystems.turret import Turret
 from utils.property import autoproperty
 from utils.safecommand import SafeCommand
 
@@ -8,27 +9,22 @@ from utils.safecommand import SafeCommand
 class Extend(SafeCommand):
     delay = autoproperty(0.3)
 
-<<<<<<< Updated upstream
-    def __init__(self, pivot: Pivot):
-        super().__init__()
-        self.pivot = pivot
-=======
-    def __init__(self, turret: Turret, speed:int, stick: wpilib.Joystick):
+
+    def __init__(self, turret:Turret, speed:int, stick: wpilib.Joystick):
         super().__init__()
         self.turret = turret
         self.stick = stick
         self.speed = speed
->>>>>>> Stashed changes
+
 
     def initialize(self):
         pass
 
-<<<<<<< Updated upstream
-=======
+
     def turn(self):
         pass
 
->>>>>>> Stashed changes
+
 
     def finished(self):
         pass
